4a18db849da8f21a68b1287c00c7465d306898e8
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-23 22:22:17 +0800

    day 3 half done

--- week-01/day3.py
+++ week-01/day3.py
@@ -2,3 +2,19 @@ a = [1, 2, 3]
 b = a           
 a.append(4)     
 print(b)
+
+b.append(99)
+
+print(id(a)) # 1761505730624
+print(id(b)) # 1761505730624
+
+a = [3, 4, 5]
+
+print(id(a)) # 2156674708544 diff id rebinded or rebound (bawal maging rebound)
+print(id(b)) # 2156672802880 same id 
+
+x = [1, 2, 3]
+y = [1, 2, 3]
+print(x is a)
+print (x == y)
+
