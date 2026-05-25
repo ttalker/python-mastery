8c51068b3ec7739466dd441082b04f849c6c2032
ttalker <165635282+ttalker@users.noreply.github.com>
2026-05-25 20:37:49 +0800

    feat: mutability traps shallow vs deep copy and slots memory comparison

---
+++ week-01/day4.py
@@ -0,0 +1,115 @@
+import time
+import tracemalloc
+
+def ex(mylist=[]):
+    mylist.append('hi')
+    print(mylist)
+
+# mylist persisting
+
+ex()
+ex()
+ex()
+
+def ey(mylist=None):
+    if mylist == None:
+        mylist = []
+    
+    mylist.append("X")
+    print(mylist)
+
+# mylist not persisting creating a new list everytime 
+ey()
+ey()
+
+# despite being immutable mutating a list inside a tuple is possible
+
+tup : tuple = ('Hello', ['X', 'Y'], "world")
+
+print(tup)
+tup[1].append("HELLO!")
+print(tup)
+
+# d = {tup: " Value"}
+
+new_tup = (1,2,3)
+
+new_d = {new_tup:"Value"}
+
+
+
+# time of +=
+
+start = time.perf_counter()
+result = ""
+for i in range(10000):
+    result += str(i)  
+
+end = time.perf_counter()
+plus_equal = end- start
+print((plus_equal) *1000)
+
+
+start = time.perf_counter()
+
+parts = []
+for i in range(10000):
+    parts.append(str(i))
+result = "".join(parts) 
+
+end = time.perf_counter()
+join = end- start
+print((join) *1000)
+
+speedup = plus_equal / join
+
+print(f"The join was {speedup} times faster")
+
+# testing memory
+
+
+class CoordDict:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+
+class CoordSlots:
+    __slots__ = ("x", "y", "z")
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+        
+
+tracemalloc.start()
+
+objs = [
+    CoordDict(i, i, i)
+    for i in range(100_000)
+]
+
+current, peak = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+mb = peak / 1024 / 1024
+
+print(f"CoordDict: {mb:.2f} MB")
+
+tracemalloc.start()
+
+objs = [
+    CoordSlots(i, i, i)
+    for i in range(100_000)
+]
+
+current, peak = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+mb = peak / 1024 / 1024
+
+print(f"CoordSlots: {mb:.2f} MB")
